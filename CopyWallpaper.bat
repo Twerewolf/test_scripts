4a3ee81beebbd28a9ad89fcc02dec8fde68427f0
jingwei tang <405146266@qq.com>
2022-01-17 19:23:57 +0800

    master

---
+++ CopyWallpaper.bat
@@ -0,0 +1,3 @@
+echo "it's a script to copy bing wallpaper to the 'picture' directory "
+xcopy /y C:\Users\ubuntu\AppData\Local\Microsoft\BingWallpaperApp\WPImages\*.jpg C:\Users\ubuntu\Pictures\BingWallpaper\test\ 
+pause
